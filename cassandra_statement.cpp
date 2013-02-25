9e64439b262c4c25ca5ec56ebe313ddd564fc5d3
sandrolex <sandrolex@gmail.com>
2013-02-25 12:13:58 -0800

    Update cassandra_statement.cpp
    adding list, set into php array and push it into php's user land.

--- cassandra_statement.cpp
+++ cassandra_statement.cpp
@@ -226,7 +226,8 @@ static pdo_param_type pdo_cassandra_get_type(const std::string &type)
         return PDO_PARAM_BOOL;
     }
 	if (!real_type.compare(0, sizeof("SetType") - 1, "SetType")
-		|| !real_type.compare(0, sizeof("MapType") - 1, "MapType")) {
+		|| !real_type.compare(0, sizeof("MapType") - 1, "MapType")
+        || (!real_type.compare(0, sizeof("ListType") - 1, "ListType"))) {
         return PDO_PARAM_ZVAL;
     }
 
@@ -245,8 +246,16 @@ static pdo_cassandra_type pdo_cassandra_get_element_type_in_collection(const std
         real_type = type;
 	if (!real_type.compare(0, 7, "SetType")) {
 		real_type = real_type.substr(sizeof("SetType"), real_type.size() - sizeof("SetType") - 1);
+        return pdo_cassandra_get_cassandra_type(real_type);
 	}
-	return pdo_cassandra_get_cassandra_type(real_type);
+
+    if (!real_type.compare(0, 8, "ListType")) {
+        real_type = real_type.substr(sizeof("ListType"), real_type.size() - sizeof("ListType") - 1);
+        return pdo_cassandra_get_cassandra_type(real_type);
+    }
+
+    return pdo_cassandra_get_cassandra_type(real_type);
+	
 }
 
 /** {{{ pdo_cassandra_type pdo_cassandra_get_cassandra_type(const std::string &type)
@@ -277,6 +286,8 @@ static pdo_cassandra_type pdo_cassandra_get_cassandra_type(const std::string &ty
         return PDO_CASSANDRA_TYPE_DOUBLE;
     if (!real_type.compare(0, 7, "SetType"))
 		return PDO_CASSANDRA_TYPE_SET;
+    if (!real_type.compare(0, 8, "ListType"))
+        return PDO_CASSANDRA_TYPE_LIST;
 
     return PDO_CASSANDRA_TYPE_UTF8;
 }
@@ -368,7 +379,7 @@ static int pdo_cassandra_stmt_describe(pdo_stmt_t *stmt, int colno TSRMLS_DC)
 /* }}} */
 
 
-void parse_collection(pdo_cassandra_type col_type, const std::string &type, const std::string &data) {
+zval* parse_collection(pdo_cassandra_type col_type, const std::string &type, const std::string &data) {
 	std::cout << "** Data type to analyse: " << type << "\t data size: " << data.size() << std::endl;
 
 	// Extract Collection and data type
@@ -376,6 +387,11 @@ void parse_collection(pdo_cassandra_type col_type, const std::string &type, cons
 	unsigned short nbElements = stream_extractor<unsigned short>(reinterpret_cast <const unsigned char *>(data.c_str()));
 
 	std::cout << "** Nb Elements in the collection: " << nbElements << std::endl;
+
+    zval *collection;
+    MAKE_STD_ZVAL(collection);
+    array_init(collection);
+
 	// Iterating trough the collection
 	const unsigned char *datap = reinterpret_cast<const unsigned char *>(data.c_str() + 2);
 	while (nbElements--)
@@ -389,19 +405,32 @@ void parse_collection(pdo_cassandra_type col_type, const std::string &type, cons
 					{
 						int value = stream_extractor<int>(datap);
 						std::cout << "Value: " << value << std::endl;
+                        add_next_index_long(collection, value);
 					}
 					break;
+                case PDO_CASSANDRA_TYPE_LONG:
+                    {
+                        long value = stream_extractor<long>(datap);
+                        add_next_index_long(collection, value);
+                    }
+                    break;
+
 				default:
 					// We consider this is a string for now :)
 					char *str = new char[elem_size + 1];
 					memcpy(str, datap, elem_size);
 					str[elem_size] = 0;
 					std::cout << "STR Value: " << str << std::endl;
+                    add_next_index_string(collection, str, 1);
 
 					break;
 				}
 			datap += elem_size;
 		}
+
+    // for a map: 
+    //add_assoc_long(collection, "lemel", 2233001);
+    return collection;
 }
 
 /** {{{ static int pdo_cassandra_stmt_get_column(pdo_stmt_t *stmt, int colno, char **ptr, unsigned long *len, int *caller_frees TSRMLS_DC)
@@ -482,16 +511,26 @@ static int pdo_cassandra_stmt_get_column(pdo_stmt_t *stmt, int colno, char **ptr
 						*caller_frees = 1;
 					}
 					break;
-				case PDO_CASSANDRA_TYPE_SET:
-				case PDO_CASSANDRA_TYPE_MAP:
+				
+				
 				case PDO_CASSANDRA_TYPE_LIST:
+                case PDO_CASSANDRA_TYPE_SET:
 					{
 						// The return value of the parse collection must be the zval stuff
-						parse_collection(lparam_type, S->result.get ()->schema.value_types [current_column], (*col_it).value);
-						// Register what has to be registered here
+                        zval *result = parse_collection(lparam_type, S->result.get ()->schema.value_types [current_column], (*col_it).value);
+                        zval **ref = (zval **) emalloc(sizeof(zval));
+                        memcpy(ref, &result, sizeof(zval)); 
+
+
+                        *ptr = (char *)ref;
+                        //*ptr = (char *) subarray;
+                        *len = sizeof(zval);
+                        *caller_frees = 1;
+
 					}
-					// TODO break once the zval is properly set
-					// break;
+                    break;
+                case PDO_CASSANDRA_TYPE_MAP:
+
                 default:
                     *ptr          = const_cast <char *> ((*col_it).value.c_str());
                     *len          = (*col_it).value.size();
