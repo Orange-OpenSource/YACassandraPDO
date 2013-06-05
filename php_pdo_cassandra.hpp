77b8770ae11711a45d525133065f7832212f325b
Mathieu Lornac <mlornac.ext@orange.com>
2013-06-05 10:28:38 +0200

    Revision update

--- php_pdo_cassandra.hpp
+++ php_pdo_cassandra.hpp
@@ -18,7 +18,7 @@
 # define _PHP_PDO_CASSANDRA_H_
 
 #define PHP_PDO_CASSANDRA_EXTNAME "pdo_cassandra"
-#define PHP_PDO_CASSANDRA_EXTVER "0.4.2"
+#define PHP_PDO_CASSANDRA_EXTVER "0.4.3"
 
 extern "C" {
 #ifdef ZTS
